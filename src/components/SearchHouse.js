d82cf2be5a7a57d0c9c28eae217f7b28ea98e2a1
nagireddykmit <73099062+nagireddykmit@users.noreply.github.com>
2024-02-08 15:29:34 +0530

    Update SearchHouse.js

--- src/components/SearchHouse.js
+++ src/components/SearchHouse.js
@@ -5,10 +5,12 @@ import House from "./House";
 const SearchHouse = (props) => {
     let param = useParams();
     let obj = props.allhouses.find((house)=>{return param.id == house._id})
+    let loggedIn = (sessionStorage.getItem("email")) ? true : false ;
+
     return ( 
         <div>
             <h1> Searched House !</h1>
-            <House houseInfo={obj} showEnquiry={true}/>
+            <House houseInfo={obj} showEnquiry={loggedIn}/>
             
         </div>
      );
